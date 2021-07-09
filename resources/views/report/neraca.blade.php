d086cbb08c85bc8c1d82a4125e4f0d1aa0836545
Esa Dandy Afriansya <esa.dandy08@gmail.com>
2021-07-09 12:23:33 +0700

    Report
    laba rugi & neraca

---
+++ resources/views/report/neraca.blade.php
@@ -0,0 +1,107 @@
+@extends('main')
+
+@section('title')
+Laporan Neraca | Ubiku Dashboard
+@endsection
+
+@section('content')
+
+<div id="wrapper">
+	<div class="main-content">
+		<div class="row small-spacing">
+
+        <div class="col-lg-12 col-xs-12">
+				<div class="box-content">
+					<h4 class="box-title">Neraca</h4>
+					<!-- /.box-title -->
+					<div class="dropdown js__drop_down">
+						<a href="#" class="dropdown-icon glyphicon glyphicon-option-vertical js__drop_down_button"></a>
+						<ul class="sub-menu">
+							
+						</ul>
+						<!-- /.sub-menu -->
+					</div>
+					<hr>
+
+					<!-- /.dropdown js__dropdown -->
+					<table class="table table-bordered">
+						<tr>
+							<td width="50%">
+								<table width="100%" class="table table-striped">
+									<tr>
+										<td>Kas</td>
+										<td>{{rupiah($kas)}}</td>
+									</tr>
+									<tr>
+										<td>Piutang</td>
+										<td>{{rupiah($piutang)}}</td>
+									</tr>
+									<tr>
+										<td>Stok</td>
+										<td>{{rupiah(0)}}</td>
+									</tr>
+									<tr>
+										<td>Biaya dibayar di muka</td>
+										<td>{{rupiah(0)}}</td>
+									</tr>
+								</table>
+							</td>
+							<td width="50%">
+								<table width="100%" class="table table-striped">
+									<tr>
+										<td>Hutang</td>
+										<td>{{rupiah($hutang)}}</td>
+									</tr>
+								</table>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<label>Aset</label>
+								<table width="100%" class="table table-striped">
+									<tr>
+										<th>Barang</th>
+										<th>Jumlah</th>
+										<th>Harga</th>
+										<th>Total</th>
+									</tr>
+									@foreach($asset AS $a)
+									<tr>
+										<td>{{$a->name}}</td>
+										<td>{{$a->quantity}}</td>
+										<td>{{rupiah($a->price)}}</td>
+										<td>{{rupiah($a->quantity*$a->price)}}</td>
+									</tr>
+									@endforeach
+								</table>
+							</td>
+							<td>
+								<table width="100%" class="table table-striped">
+									<tr>
+										<td>Laba Ditahan</td>
+										<td>{{rupiah(0)}}</td>
+									</tr>
+									<tr>
+										<td>Laba Berjalan</td>
+										<td>{{rupiah(0)}}</td>
+									</tr>
+								</table>
+							</td>
+						</tr>
+					</table>
+				</div>
+				<!-- /.box-content -->
+			</div>
+    
+        </div>
+		
+		<!-- /.row -->		
+		@include('footer')
+	</div>
+	<!-- /.main-content -->
+</div><!--/#wrapper -->
+@endsection
+
+@section('js')
+<script src="{{ url('js/account/account.js') }}"></script>
+@endsection
